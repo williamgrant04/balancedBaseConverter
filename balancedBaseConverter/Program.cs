578bd99db66c84721104fb14e6dc4ee6d73bb17b
Cork <williamg3575467@gmail.com>
2022-01-25 10:44:53 +0000

    Add project files.

---
+++ balancedBaseConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using corklibrary;
+
+namespace balancedBaseConverter {
+    class Program {
+        static void Main(string[] args) {
+            int[] list = {};
+            list = list.RemoveAt(0);
+            
+            foreach (int item in list)
+                Console.Write(item);
+        }
+    }
+}
---
+++ corklibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace corklibrary {
+    public static class ArrayC {
+        public static T[] RemoveAt<T>(this T[] arg,int index) {
+            List<T> list = arg.ToList();
+
+            if (arg.Length <= 0) 
+                throw new ArgumentNullException($"Cannot remove item at index {index} from null array.");
+            if (index < 0 || index >= list.Count) 
+                throw new IndexOutOfRangeException("Attempt to remove item from array at index " + index + ". Index does not exist within array.");
+
+            list.RemoveAt(index);
+            return list.ToArray();
+        }
+    }
+}
