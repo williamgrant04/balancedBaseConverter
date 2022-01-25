728f1d4d335804b080f7039133100c03dd7af950
Cork <williamg3575467@gmail.com>
2022-01-25 13:58:47 +0000

    Completed ToBase started work in ToBalancedBase.

--- balancedBaseConverter/Program.cs
+++ balancedBaseConverter/Program.cs
@@ -4,13 +4,38 @@ using System.Collections.Generic;
 using corklibrary;
 
 namespace balancedBaseConverter {
-    class Program {
+    static class Program {
         static void Main(string[] args) {
-            int[] list = {};
-            list = list.RemoveAt(0);
-            
-            foreach (int item in list)
-                Console.Write(item);
+            //Console.Write("Enter a base: ");
+            //int eBase = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number to convert: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(number.ToBase(2));
+        }
+
+        public static int ToBase(this int num, int eBase, bool balanced = false) {
+            char[] number = num.ToArray();
+            if (number[0] == '-') {
+                throw new ArgumentException("Number cannot be negative, try ToBalancedBase instead.");
+            }
+
+            List<int> converted = new List<int>();
+
+            while (num != 0) {
+                converted.Add(num % eBase);
+                num /= eBase;
+            }
+
+            if (balanced) {
+                return num.ToBalancedBase(eBase);
+            } else {
+                return Convert.ToInt32(converted);
+            }
+
+        }
+
+        public static int ToBalancedBase(this int num, int enBase) {
+
         }
     }
 }
--- corklibrary/Class1.cs
+++ corklibrary/Class1.cs
@@ -5,13 +5,16 @@ namespace corklibrary {
         public static T[] RemoveAt<T>(this T[] arg,int index) {
             List<T> list = arg.ToList();
 
-            if (arg.Length <= 0) 
-                throw new ArgumentNullException($"Cannot remove item at index {index} from null array.");
             if (index < 0 || index >= list.Count) 
                 throw new IndexOutOfRangeException("Attempt to remove item from array at index " + index + ". Index does not exist within array.");
 
             list.RemoveAt(index);
             return list.ToArray();
+        }
+
+        public static char[] ToArray(this int num) {
+            string str = num.ToString();
+            return str.ToArray();
         }
     }
 }
